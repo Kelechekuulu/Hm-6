856251aa36555a503c89835255b8caacbea3bc79
Nurpeiil <nurpeiilk99@gmail.com>
2023-04-17 17:00:30 +0600

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Expenses from "./components/expenses/Expenses";
+import NewExpense from "./components/newExpense/NewExpense";
+
+const productData = [
+  {
+    title: "Alma",
+    price: 120,
+    date: "01.25.23",
+    id: "1",
+  },
+
+  {
+    title: "Food",
+    price: 380,
+    date: "11.07.23",
+    id: "2",
+  },
+
+  {
+    title: "Juice",
+    price: 80,
+    date: "05.15.23",
+    id: "3",
+  },
+];
 
 function App() {
+  const [newProduct, setNewProduct] = useState(productData);
+
+  function addExpenseHandler(data) {
+    setNewProduct([...newProduct, data]);
+  }
+
+  function deleteExpense(id) {
+    const newData = newProduct.filter((el) => el.id !== id);
+    setNewProduct(newData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense addExpenseHandler={addExpenseHandler} />
+      <Expenses
+        data={newProduct}
+        setNewProduct={setNewProduct}
+        onDelete={deleteExpense}
+      />
     </div>
   );
 }
